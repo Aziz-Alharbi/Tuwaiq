6d548e057fee28f0e2684ba33394c2f9f740bec4
Aziz-Alharbi <ayh92000@gmail.com>
2025-07-15 13:51:38 +0300

    Finished the intro to Spring boot

---
+++ demo_day_18/src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Aziz !";
+    }
+
+    @GetMapping("/name")
+    public String name(){
+        return "My name is Abdulaziz Alharbi";
+    }
+
+    @GetMapping("/age")
+    public String age(){
+
+        return "My age is 30";
+    }
+
+    @GetMapping("/check/status")
+    public String req(){
+
+        return "Everything OK";
+    }
+
+    @GetMapping("/health")
+    public String health(){
+
+        return "Server health is up and running";
+    }
+
+    @GetMapping("/names")
+    public String[] rist(){
+        String names[] = {"Aziz", "Mohammed"};
+
+        return names;
+    }
+
+
+}
